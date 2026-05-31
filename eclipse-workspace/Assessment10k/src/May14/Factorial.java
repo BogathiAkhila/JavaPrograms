9b07dad1795ed28c22cec871c98580e65b888af5
Your Name <akhila221202@gmail.com>
2026-05-31 14:35:10 +0530

    Added Factorial.java

---
+++ eclipse-workspace/Assessment10k/src/May14/Factorial.java
@@ -0,0 +1,20 @@
+package May14;
+import java.util.Scanner;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number");
+		int n=sc.nextInt();
+		int count=1;
+		for(int i=n;i<=n;i--) {
+			count*=i;
+			
+		}
+		System.out.println(count);
+
+	}
+
+}
